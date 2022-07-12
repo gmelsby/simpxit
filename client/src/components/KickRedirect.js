dac14d95612da979230a6c47720058acf37bdd78
Gregory Melsby <greg27426@gmail.com>
2022-07-12 15:01:16 -0500

    fixed typo

--- client/src/components/KickRedirect.js
+++ client/src/components/KickRedirect.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Alert } from 'react-bootstrap';
 
