906325f19c5b38c259d835c86b72c8bbf4b5a9df
Gregory Melsby <greg27426@gmail.com>
2022-08-04 14:33:02 -0500

    added carousel for xs screens scoring section

--- client/src/components/ScoringCardHand.js
+++ client/src/components/ScoringCardHand.js
@@ -1,4 +1,4 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Carousel } from "react-bootstrap";
 import ScoringCard from './ScoringCard';
 import React from 'react';
 
@@ -13,20 +13,30 @@ export default function ScoringCardHand({storyTeller,
     return players.filter(p => guesses[p.playerId] === cardId);
   }                                          
 
-  const nonStoryTellers = players.filter(p => p.playerId !== storyTeller.playerId);
+  const sortedPlayers = players.sort((p1, p2) => p1.playerId === storyTeller.playerId ? -1 : 1)
+  const ScoringCards = sortedPlayers.map(p =>
+          <ScoringCard key={p.playerId} player={p} card={submittedCards[p.playerId]}
+            guessedPlayers={playersWhoGuessed(submittedCards[p.playerId].cardId)} 
+            isStoryTeller={p.playerId === storyTeller.playerId} />
+        );
+  
 
   return (
-    <Row xs={1} md={Math.min(3, Object.values(submittedCards).length)} className="g-2 mx-5 my-3">
-      <Col>
-        <ScoringCard player={storyTeller} card={submittedCards[storyTeller.playerId]} 
-          guessedPlayers={playersWhoGuessed(submittedCards[storyTeller.playerId].cardId)}
-          isStoryTeller />
-      </Col>
-      {nonStoryTellers.map(p =>
-        <ScoringCard key={p.playerId} player={p} card={submittedCards[p.playerId]}
-          guessedPlayers={playersWhoGuessed(submittedCards[p.playerId].cardId)} />
+    <>
+      <Row xs={1} md={Math.min(3, Object.values(submittedCards).length)} className="g-2 mx-5 my-3 d-none d-sm-flex">
+        {ScoringCards}
+      </Row>
+
+      <Carousel className="d-xs-flex d-sm-none" interval={null} variant="dark">
+      {ScoringCards.map(card => 
+        <Carousel.Item key={card.cardId}> 
+          <Row className="mx-5">
+            {card}
+          </Row>
+        </Carousel.Item>
       )}
-    </Row>
+      </Carousel>
+    </>
   )
 
 }
