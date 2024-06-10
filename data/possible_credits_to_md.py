200fb03dd281bec23314de6b6910549c3f5033e2
Gregory Melsby <greg27426@gmail.com>
2024-06-10 13:40:02 -0400

    possible_credits_to_md.py strips whitespace on end of filenames

--- data/possible_credits_to_md.py
+++ data/possible_credits_to_md.py
@@ -3,8 +3,9 @@ import sys
 folder_name = '.images'
 
 def main():
-    for file_name in sys.stdin:
-        if not file_name.strip():
+    for line in sys.stdin:
+        file_name = line.strip()
+        if not file_name:
             break
         print(f'![{file_name}]({folder_name}/{file_name})')
 
