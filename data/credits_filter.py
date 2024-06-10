23b8b0a7724cb8a3c25ad09af1db72a7b88673c4
Gregory Melsby <greg27426@gmail.com>
2024-06-10 13:02:27 -0400

    credits_filter iterates through all images

--- data/credits_filter.py
+++ data/credits_filter.py
@@ -1,14 +1,32 @@
 from PIL import Image #type: ignore
 import numpy as np #type: ignore
 from pathlib import Path
+import sys
 
-image_path = 'images/S03E22_295585.webp'
 
-with Image.open(image_path, 'r').convert('L') as image:
-  img_arr = np.array(image)
-  # gets dimensions of image
-  pixel_count = len(img_arr) * len(img_arr[0])
-  # filters for pictures with greyscale value < 10
-  dark_pixels = np.count_nonzero(img_arr < 10)
+def dark_pixel_percentage(image_path: str, pixel_value_cutoff: int=10) -> float:
+    """
+    Returns the percentage of dark pixels in the image at image_path 
+    A pixel is considered dark if its greyscale value is < pixel_value_cutoff
+    """
+    with Image.open(image_path, 'r').convert('L') as image:
+        img_arr = np.array(image)
+        # gets dimensions of image
+        pixel_count = len(img_arr) * len(img_arr[0])
+        # filters for pictures with greyscale value < 10
+        dark_pixels = np.count_nonzero(img_arr < pixel_value_cutoff)
+        
+        return dark_pixels / pixel_count * 100
 
-  print(f'{round(dark_pixels / pixel_count * 100, 2)}% dark pixels')
+def main(args):
+    percent_threshold = int(args[1]) if len(args) > 1 else 70
+    pixel_cutoff = int(args[2]) if len(args) > 2 else 10
+
+    image_paths = Path('images').glob('*.webp')
+    for image in image_paths:
+        percentage = dark_pixel_percentage(image, pixel_cutoff)
+        if percentage >= percent_threshold:
+            print(Path(image).name)
+
+if __name__ == '__main__':
+    main(sys.argv)
