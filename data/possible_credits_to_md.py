3828d692888faa0d325096c5b3c183dba06241a0
Gregory Melsby <greg27426@gmail.com>
2024-06-10 13:37:03 -0400

    possible_credits_to_md reads from standard input for piping

--- data/possible_credits_to_md.py
+++ data/possible_credits_to_md.py
@@ -3,11 +3,10 @@ import sys
 folder_name = '.images'
 
 def main():
-    with open(sys.stdin, 'r') as possible_credits:
-        file_name = possible_credits.readline()
-        while file_name:
-            print(f'![{file_name}]({folder_name}/{file_name})')
-            file_name = possible_credits.readline()
+    for file_name in sys.stdin:
+        if not file_name.strip():
+            break
+        print(f'![{file_name}]({folder_name}/{file_name})')
 
 if __name__ == '__main__':
     main()
