e31efbe4e3517019d197332409877c57bb2233b6
Gregory Melsby <greg27426@gmail.com>
2024-06-12 18:00:42 -0400

    uses boto3.client instead of boto3.resource, also checks if object resists before reuploading

--- data/s3_uploader.py
+++ data/s3_uploader.py
@@ -1,4 +1,5 @@
 import boto3 #type: ignore
+from botocore.exceptions import ClientError #type: ignore
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv #type: ignore
@@ -15,7 +16,7 @@ region: str = os.environ.get("S3_REGION")
 access_key: str = os.environ.get("S3_ACCESS_KEY")
 secret_key: str = os.environ.get("S3_SECRET_KEY")
 
-s3 = boto3.resource(
+s3 = boto3.client(
     's3',
     aws_access_key_id=access_key,
     aws_secret_access_key=secret_key,
@@ -24,15 +25,21 @@ s3 = boto3.resource(
 )
 
 def main():
-    bucket = s3.Bucket(bucket_name)
     image_paths = Path(folder_name).glob('*.webp')
-    count = 0
+    uploaded_count = 0
+    existing_count = 0
     for i, image in enumerate(image_paths):
-        bucket.upload_file(str(image), Path(image).name, ExtraArgs={'ContentType': 'image/webp'})
-        count = i + 1
-        print(f'  {('\\', '|', '/', '|')[i % 4]} uploaded {count} files', end='\r')
+        try:
+            s3.head_object(Bucket=bucket_name, Key=str(image.name))
+            existing_count += 1
+        except ClientError:
+            s3.upload_file(str(image), bucket_name, str(image.name), ExtraArgs={'ContentType': 'image/webp'})
+            uploaded_count += 1
 
-    print(f'uploaded {count} files')
+        print(f'  {('\\', '|', '/', '|')[i % 4]} {uploaded_count} files uploaded | {existing_count} files already in bucket | {existing_count + uploaded_count} total files processed', end='\r')
+
+    print(f'{uploaded_count + existing_count} files processed')
+
     
 if __name__ == '__main__':
     main()
