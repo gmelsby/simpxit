1ea2af0f6b917b4e221ff71043c815153b302710
Gregory Melsby <greg27426@gmail.com>
2024-06-27 11:36:41 -0400

    updated typescript interfaces for socketio

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
+import { JSONPatchOperation } from 'immutable-json-patch';
+
 export interface CardInfo {
   episode_key: string;
   season: number;
@@ -44,4 +46,27 @@ export interface Room {
   playerTurn: number;
   readyForNextRound: string[];
   lastModified: number;
+  updateCount: number;
+}
+
+// Socket.IO types
+export interface ServerToClientEvents {
+  receiveRoomState: (room: Room) => void;
+  receiveRoomPatch: ({operations, updateCount}: {operations: JSONPatchOperation[], updateCount: number}) => void;
+  resetRoundValues: (updateCount: number) => void;
+  resetRoundValues: (updateCount: number) => void;
+}
+
+export interface ClientToServerEvents {
+  requestRoomState: ({roomId, userId}: {roomId: string, userId: string}, callback: (e: string) => void) => void;
+  joinRoom: ({roomId, userId}: {roomId: string, userId: string}, callback: (e: string) => void) => void;
+  kickPlayer: ({roomId, userId, kickUserId}: {roomId: string, userId: string, kickUserId: string}) => void;
+  leaveRoom: ({roomId, userId}: {roomId: string, userId: string}, callback: (e: string) => void) => void;
+  changeName: ({roomId, userId, newName}: {roomId: string, userId: string, newName: string}) => void;
+  changeOptions: ({roomId, userId, newOptions}: {roomId: string, userId: string, newOptions: number}) => void;
+  startGame: ({roomId, userId}: {roomId: string, userId: string}) => void;
+  submitStoryCard: ({roomId, userId, selectedCardId, descriptor}: {roomId: string, userId: string, selectedCardId: string, descriptor: string}) => void;
+  submitOtherCard: ({roomId, userId, selectedCardId}: {roomId: string, userId: string, selectedCardId: string}) => void;
+  guess: ({roomId, userId, selectedCardId}: {roomId: string, userId: string, selectedCardId: string}) => void;
+  endScoring: ({roomId, userId}: {roomId: string, userId: string}) => void;
 }
