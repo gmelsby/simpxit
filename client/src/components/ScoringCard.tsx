6d64e4f8be2482c7f4a4f122eebbe48e9edecbe7
Gregory Melsby <greg27426@gmail.com>
2024-06-29 17:44:12 -0400

    basic scoring card info display updated, shows number og guesses on card

--- client/src/components/ScoringCard.tsx
+++ client/src/components/ScoringCard.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Card as BootstrapCard, Col, } from 'react-bootstrap';
+import React, { useCallback } from 'react';
+import { Card as BootstrapCard, Col, Row } from 'react-bootstrap';
 import { Player, GameCard } from '../../../types';
 
-export default function ScoringCard({ player, card, /*guessedPlayers,*/ isStoryTeller }:
+export default function ScoringCard({ player, card, guessedPlayers, isStoryTeller }:
   {player: Player, card: GameCard, guessedPlayers: Player[], isStoryTeller: boolean}) {
   /*const GuessedInfo = () => {
     return (
@@ -14,12 +14,30 @@ export default function ScoringCard({ player, card, /*guessedPlayers,*/ isStoryT
     );
   };*/
 
+  // returns the html code for the number in a cicle for numbers 1-8
+  const circleNumberHtmlCalc = useCallback(((n: number) => String.fromCharCode(0x245F + n)), []);
+
   return (
     <Col>
-      <BootstrapCard className="card-img">
-        <BootstrapCard.Img variant="top" src={card.locator} className="card-img"/>
+      <BootstrapCard className="card-img selectable-no-grow">
+        <BootstrapCard.Img variant="bottom" src={card.locator} className="card-img"/>
         <BootstrapCard.Body>
-          <BootstrapCard.Title>Submitted by {isStoryTeller && 'Storyteller '}{!isStoryTeller && player.playerName}</BootstrapCard.Title>
+          <Row className='justify-content-center flex-nowrap'>
+            <Col></Col>
+            <Col xs={6} md={8}>
+              <Row clasName='pb-0 mb-0'>
+                <p className='mb-0'>Submitted by</p>
+              </Row>
+              <Row className='pt-0 mt-0'>
+                <h5 className='mt-1'>{isStoryTeller && 'Storyteller '}{!isStoryTeller && player.playerName}</h5>
+              </Row>
+            </Col>
+            <Col className='d-flex flex-column justify-content-center'>
+              {guessedPlayers.length !== 0 &&
+                <h2><b>{circleNumberHtmlCalc(guessedPlayers.length)}</b></h2>
+              }
+            </Col>
+          </Row>
         </BootstrapCard.Body>
       </BootstrapCard>
     </Col>
