4590bd77fbf17ae9891226a32c40c2d96d3c380b
Gregory Melsby <greg27426@gmail.com>
2024-07-04 17:02:55 -0400

    modified JustifyEvenlyContainer to take a parameter to specify non-overflow justify type

---
+++ client/src/components/JustifySafelyContainer.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import { Container } from 'react-bootstrap';
+
+
+// justifies according to justifyType unless oveflow is detected, in which case it will justify content start
+export default function JustifySafelyContainer({justifyType, className, children}: 
+  {justifyType: 'center' | 'evenly', className: string, children: ReactNode}) {
+
+  const containerElement = useRef<HTMLDivElement>(null);
+  const [overflowing, setOverflowing] = useState(false);
+
+  useEffect(() => {
+    const checkOverflowing = () => {
+      if (containerElement.current !== null) {
+        setOverflowing(containerElement.current.scrollHeight > containerElement.current.offsetHeight);
+      }
+    };
+
+    // on component load
+    checkOverflowing();
+
+    const observer = new ResizeObserver(() => checkOverflowing);
+    // keep updating every time a resize happens
+    if (containerElement.current !== null) {
+      observer.observe(containerElement.current);
+    }
+    // check for window resize too
+    window.addEventListener('resize', checkOverflowing);
+    // cleanup
+    return (() => {
+      window.removeEventListener('resize', checkOverflowing);
+      observer.disconnect();
+    });
+  }, [containerElement.current]);
+
+  return (
+    <Container 
+      className=
+        {`${overflowing ? 
+          'justify-content-start overflow-visible' : 
+          `justify-content-${justifyType}`
+        }
+      ${className}`}
+      ref={containerElement}
+    >
+      {children}
+    </Container>
+  );
+}
