828ca44bfe22800ebee94dd7772df6c0fb3a8535
Gregory Melsby <greg27426@gmail.com>
2024-07-10 12:51:33 -0400

    made index.css have dark background color to avoid flashing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
-// gets bootstrap style sheet
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
