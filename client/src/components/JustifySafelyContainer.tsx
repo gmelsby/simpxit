e6ce2383fda89b312b80f431c729631b9c0dd9e8
Gregory Melsby <greg27426@gmail.com>
2024-08-20 17:09:39 -0400

    added second timeout that re-checks overflowing in case rendering is slower than first check

--- client/src/components/JustifySafelyContainer.tsx
+++ client/src/components/JustifySafelyContainer.tsx
@@ -1,23 +1,31 @@
 import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
-
 // justifies according to justifyType unless oveflow is detected, in which case it will justify content start
-export default function JustifySafelyContainer({justifyType, overflowType, className, children, containerRef}: 
-  {
-    justifyType: 'center' | 'evenly', 
-    overflowType?: 'auto' | 'hidden' | 'visible', 
-    className: string, children: ReactNode, 
-    containerRef?: React.MutableRefObject<HTMLDivElement | null>
-  }) {
-
-  const containerElement = containerRef !== undefined ? containerRef : useRef<HTMLDivElement>(null);
+export default function JustifySafelyContainer({
+  justifyType,
+  overflowType,
+  className,
+  children,
+  containerRef,
+}: {
+  justifyType: 'center' | 'evenly';
+  overflowType?: 'auto' | 'hidden' | 'visible';
+  className: string;
+  children: ReactNode;
+  containerRef?: React.MutableRefObject<HTMLDivElement | null>;
+}) {
+  const containerElement =
+    containerRef !== undefined ? containerRef : useRef<HTMLDivElement>(null);
   const [overflowing, setOverflowing] = useState(false);
 
   useEffect(() => {
     const checkOverflowing = () => {
       if (containerElement.current !== null) {
-        setOverflowing(containerElement.current.scrollHeight > containerElement.current.offsetHeight);
+        setOverflowing(
+          containerElement.current.scrollHeight >
+            containerElement.current.offsetHeight,
+        );
       }
     };
 
@@ -25,6 +33,7 @@ export default function JustifySafelyContainer({justifyType, overflowType, class
     checkOverflowing();
     // check again after event loop has had some time
     const timeout = setTimeout(checkOverflowing, 100);
+    const timeout2 = setTimeout(checkOverflowing, 500);
 
     const observer = new ResizeObserver(() => checkOverflowing);
     // keep updating every time a resize happens
@@ -34,20 +43,21 @@ export default function JustifySafelyContainer({justifyType, overflowType, class
     // check for window resize too
     window.addEventListener('resize', checkOverflowing);
     // cleanup
-    return (() => {
+    return () => {
       clearTimeout(timeout);
+      clearTimeout(timeout2);
       window.removeEventListener('resize', checkOverflowing);
       observer.disconnect();
-    });
+    };
   }, [overflowType, containerElement.current]);
 
   return (
-    <Container 
-      className=
-        {`${overflowing ? 
-          `justify-content-start overflow-${overflowType ? overflowType : 'visible'}` :
-          `justify-content-${justifyType}`
-        }
+    <Container
+      className={`${
+        overflowing
+          ? `justify-content-start overflow-${overflowType ? overflowType : 'visible'}`
+          : `justify-content-${justifyType}`
+      }
       ${className}`}
       ref={containerElement}
     >
