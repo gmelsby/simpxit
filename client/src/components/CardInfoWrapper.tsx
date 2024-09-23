4ef0180d28eddaa843aacc3a9c853f733b21e861
Gregory Melsby <greg27426@gmail.com>
2024-09-23 13:35:10 -0400

    temporarily buffers loaded card info with a transparent display to prevent flashing when JustifySafelyContainer does not respond in time

--- client/src/components/CardInfoWrapper.tsx
+++ client/src/components/CardInfoWrapper.tsx
@@ -5,11 +5,14 @@ import CardInfoText from './CardInfoText';
 export default function CardInfoWrapper({ cardId, load }: { cardId: string, load: boolean }) {
   // 'load' functions as a trigger for intial load. This helps enables lazy loading
   const [cardInfo, setCardInfo] = useState(undefined);
+  const [recentlyLoaded, setRecentlyLoaded] = useState(false);
 
   const loadCardInfo = useCallback(async () => {
     const response = await fetch(`/api/cardinfo/${cardId}`);
     const data = await response.json();
     setCardInfo(data);
+    setRecentlyLoaded(true);
+    setTimeout(() => setRecentlyLoaded(false), 200);
   }, [cardId]);
 
   // only try to get card info if we don't already have it
@@ -19,5 +22,9 @@ export default function CardInfoWrapper({ cardId, load }: { cardId: string, load
     }
   }, [cardId, cardInfo, loadCardInfo, load]);
 
-  return (<CardInfoText cardInfo={cardInfo} />);
+  return (
+    <div className={recentlyLoaded ? 'opacity-0' : ''}>
+      <CardInfoText cardInfo={cardInfo} />
+    </div>
+  );
 }
